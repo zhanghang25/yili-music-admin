5295e44066b16d043d19f96a12ce21d16e9cc168
张航 <672683505@qq.com>
2022-07-12 10:17:59 +0800

    quasar  admin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,28 @@
 <script setup>
 import { ref } from 'vue'
+import { sayHello } from "../api/hello";
 
 defineProps({
   msg: String
 })
-
 const count = ref(0)
+const message = ref("您哈,quasar!")
+
+const hi = () =>{
+  sayHello().then(res=>{
+    console.log(res)
+    message.value = res.data
+  })
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h4>{{ message }}</h4>
+  <q-btn color="primary" @click="count++">点击次数 {{count}}</q-btn>
+  <br>
+  <br>
+  <q-btn @click="hi" >获取请求</q-btn>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, TransformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue({
+      template: TransformAssetUrls
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ]
 })
